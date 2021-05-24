aaf062b050f3ba07efe39fb3be322c1aaceb6a05
Lakshman Tiwari <laxman_tiwari123@hotmail.com>
2021-05-24 21:38:39 +0530

    Create PolyMul.cpp

---
+++ PolyMul.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+using cd = complex<double>;
+const double PI = acos(-1);
+inline ll scan()
+    {
+        ll z=0;
+        char c;
+        do{ c=getchar_unlocked(); } while(c<'0');
+        for(;c>='0';c=getchar_unlocked()) z = (z<<3) + (z<<1) + (c&15);
+        return z;
+    }
+inline void put_uint(ll n) {
+  char stack[22];
+  int top = 0;
+  if(n == 0) {
+    putchar_unlocked('0');
+  } else {
+    while(n > 0) {
+      stack[top++] = n % 10 + '0';
+      n /= 10;
+    }
+    while(top > 0) {
+      putchar_unlocked(stack[--top]);
+    }
+  }
+  putchar_unlocked(' ');
+}
+
+void fft(vector<cd> & a, bool invert) {
+    int n = a.size();
+
+    for (int i = 1, j = 0; i < n; i++) {
+        int bit = n >> 1;
+        for (; j & bit; bit >>= 1)
+            j ^= bit;
+        j ^= bit;
+
+        if (i < j)
+            swap(a[i], a[j]);
+    }
+
+    for (int len = 2; len <= n; len <<= 1) {
+        double ang = 2 * PI / len * (invert ? -1 : 1);
+        cd wlen(cos(ang), sin(ang));
+        for (int i = 0; i < n; i += len) {
+            cd w(1);
+            for (int j = 0; j < len / 2; j++) {
+                cd u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w *= wlen;
+            }
+        }
+    }
+
+    if (invert) {
+        for (cd & x : a)
+            x /= n;
+    }
+}
+
+vector<ll> multiply(vector<ll> const& a, vector<ll> const& b) {
+    vector<cd> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+    int n = 1;
+    while (n < a.size() + b.size()) 
+        n <<= 1;
+    fa.resize(n);
+    fb.resize(n);
+
+    fft(fa, false);
+    fft(fb, false);
+    for (int i = 0; i < n; i++)
+        fa[i] *= fb[i];
+    fft(fa, true);
+
+    vector<ll> result(n);
+    for (int i = 0; i < n; i++)
+        result[i] = round(fa[i].real());
+    return result;
+}
+
+int main(){
+	int t ,n;
+	t = scan();
+	for(int cs = 1; cs <= t; cs++){
+		n = scan();
+		vector<ll> a(n+1), b(n+1);
+		
+		for(int i=0; i<=n; i++) a[i]=scan();	
+		for(int i=0; i<=n; i++) b[i]=scan();	
+		
+		vector<ll> res = multiply(a, b);
+		for (ll i = 0; i <= 2 * n; ++i){
+			put_uint(res[i]);
+		}
+		cout <<endl;
+	}
+}
